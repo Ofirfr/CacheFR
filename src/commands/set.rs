0f1258a2d98d1f086f92f42aa84b6e755a12c83e
Ofirfr <ofir.frisch1@gmail.com>
2024-06-07 19:26:37 +0300

    optimize set function

--- src/commands/set.rs
+++ src/commands/set.rs
@@ -2,23 +2,22 @@ use crate::{
     commands_proto::{FrKey, FrValue},
     value_structs::{CacheFRMap, StoredFrValueWithExpiry},
 };
-pub async fn set_value_in_map<'a>(
+
+pub async fn set_value_in_map(
     main_map: &CacheFRMap,
     key: FrKey,
     value: FrValue,
     only_if_not_exists: bool,
-) -> Result<FrValue, String> {
+) -> Result<(), String> {
     // Check only-if-exists-constraint
     if only_if_not_exists && main_map.contains_key(&key) {
         return Err("Key already exists".to_string());
     }
+    let value_to_store = StoredFrValueWithExpiry::from_fr_value(value);
 
     // Write block for minimal blocking time
     {
-        main_map.insert(
-            key.clone(),
-            StoredFrValueWithExpiry::from_fr_value(value.clone()),
-        );
+        main_map.insert(key, value_to_store);
     }
-    return Ok(value);
+    Ok(())
 }
